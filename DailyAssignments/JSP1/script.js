ba8a539e12891640bef8c0928a93eb6a701e5fb3
Syed Danish Pasha <sydanishp@gmail.com>
2022-12-30 02:09:02 +0530

    JS1

---
+++ DailyAssignments/JSP1/script.js
@@ -0,0 +1,7 @@
+var studentDetails = 'Danish', designation = 'Full Stack Developer', age = 24, salary = 37000, city = 'Bangalore';
+console.log(studentDetails, designation, age, salary, city);
+document.getElementById('stud').innerHTML = studentDetails;
+document.getElementById('desi').innerHTML = designation;
+document.getElementById('age').innerHTML = age;
+document.getElementById('sal').innerHTML = salary;
+document.getElementById('cit').innerHTML = city;
---
+++ JavaScript/myScript.js
@@ -0,0 +1,54 @@
+// Type of JS O/P
+
+// alert('You have successfully Logged In')
+
+//  document.write('<h1>Welcome User</h1>')
+
+// console.log('User'); //Specific used for debugging only 
+
+
+// var d = 500 + 100 + ('Users');
+// console.log(d);
+
+// document.getElementById('use');
+
+// Variables
+// var is a declaration for string
+var a = "Hello";//Alphabets initialization should be inside " "
+var b = 150;//Numbers initialization can be given directly
+
+var x;//Declaration
+x = 'Alpha';//Initalization
+
+var z;
+z = 20;
+
+// Muliple varibales in single line
+
+var userDeatils = 'Danish', age = 24;
+
+console.log(b, z);
+// alert(a);
+// document.write(a + x);
+document.getElementById('deatils').innerHTML = userDeatils;
+
+var tax = 20;
+var food = 1500;
+
+var total = tax + food;
+
+// Numbers
+var numOne = 100;
+var numTwo = 150.23;
+
+console.log(typeof(numOne));
+console.log(typeof(numTwo));
+console.log(typeof(a));
+
+//Extra Information
+// var r = confirm("Like to go ahead");
+// console.log(r)
+
+// var e = prompt("Enter Your Name");
+// console.log(e);
+
